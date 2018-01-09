810db0a42c3e78a65b75849cbeb30eaf28ea38f2
alecgunnar <alecgunnar@gmail.com>
2018-01-08 20:36:19 -0500

    Adds sass and moves scss into own file

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,11 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-a {
-  color: #2c3e50;
-}
+<style lang="scss">
+  @import "assets/scss/global.scss";
 </style>
