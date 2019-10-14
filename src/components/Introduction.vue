e9e92116c30127f2a0b2e0b8cda4c39d0c8478e8
Alec Carpenter <alecgunnar@gmail.com>
2019-10-13 19:59:48 -0600

    Updates portrait photo to be georgetown lake

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="introduction">
     <section class="portrait">
-      <div class="portrait__description">Zion National Park, Utah, U.S.A.</div>
+      <div class="portrait__description">Looking north across Georgetown Lake in Georgetown, Colorado, U.S.A.</div>
     </section>
     <header class="me">
       <div class="me__photoFrame">
@@ -25,7 +25,7 @@ export default {
 <style lang="scss">
   .portrait {
     background-color: #000;
-    background-image: url("../assets/images/zion_river.png");
+    background-image: url("../assets/images/georgetown_lake.jpg");
     background-repeat: no-repeat;
     background-size: contain;
     padding-bottom: 75%;
